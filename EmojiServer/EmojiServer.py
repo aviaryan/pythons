49417914a848de7313e4870de823d19ca7f728fc
aviaryan <avi.aryan123@gmail.com>
2015-09-02 01:54:54 +0530

    added EmojiServer

---
+++ EmojiServer/EmojiServer.py
@@ -0,0 +1,47 @@
+import http.server
+import socketserver
+from subprocess import call
+from platform import system
+from sys import exit as sysexit
+
+
+######## CONFIG
+
+if system() == 'Windows':
+	browser = ''
+elif system() == 'Linux':
+	browser = 'xdg-open'
+elif system() == 'darwin':
+	browser = 'open'
+else:
+	print('What OS ? Please add the browser manually')
+	sysexit()
+
+
+# In Windows, set browser to empty to use default browser
+PORT = 8001
+
+######## PROGRAM
+
+def main():
+	Handler = http.server.SimpleHTTPRequestHandler
+
+	httpd = socketserver.TCPServer(("", PORT), Handler)
+	qStr = browser + " http://localhost:" + str(PORT)
+
+	if system() == 'Windows':
+		call("start " + qStr , shell=True)
+	elif system() == 'Linux':
+		call(qStr)
+	elif system() == 'darwin':
+		call(qStr)
+	else:
+		call(qStr)
+
+	print("serving at port", PORT)
+	print("Use ^C to exit")
+	httpd.serve_forever()
+
+
+if __name__ == "__main__":
+	main()
---
+++ EmojiServer/README.md
@@ -0,0 +1,17 @@
+# Emoji Server
+
+A simple script that helps you run [muan/emoji](https://github.com/muan/emoji) offline.
+
+
+## Setting Up
+
+* Download zip(source) from [muan/emoji](https://github.com/muan/emoji). Place it in the same folder as `EmojiServer.py`. That is, EmojiServer.py and index.html should be in the same folder.
+
+
+## Using
+
+* Run `EmojiServer.py`
+* The script should create the local server and open the browser automatically.
+
+
+**Note** - You can change the default browser in the script's config section.
