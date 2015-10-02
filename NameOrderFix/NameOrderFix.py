d90a9665bcdd8e2ac04cde3f3d687618151166c2
aviaryan <avi.aryan123@gmail.com>
2015-10-02 11:34:50 +0530

    better NameOrderFix, fixed issue with numbers in series names

--- NameOrderFix/NameOrderFix.py
+++ NameOrderFix/NameOrderFix.py
@@ -3,7 +3,7 @@ import re
 
 ############# CONFIG ##############
 
-# the nth number in filename to pad
+# the nth number in the filename to pad
 match_index = 0
 # the padding you want to add. For 3, 1 becomes 001 and 99 becomes 099
 pad_zeros = 3
@@ -11,16 +11,28 @@ pad_zeros = 3
 ###################################
 
 
-path = input('Enter the full path of the folder whose contents you want to fix order: \n> ')
+path = input('Enter the full path of the folder whose contents order you want to have fixed: \n> ')
 print()
 fstr = '{:0' + str(pad_zeros) + 'd}'
 store = {}
+pattern = re.compile('(\d+)')
 
 for i in os.listdir(path):
-	nums = re.findall('\d+', i, re.DOTALL)
-	newname = i.replace(nums[match_index], fstr.format( int(nums[match_index]) ), 1)
-	print(i, '>>' , newname)
-	store[i] = newname
+	j = 0
+	s,e = -1,-1
+	for m in pattern.finditer(i):
+		if j == match_index:
+			item = m.group()
+			s = m.start()
+			e = m.end()
+			break
+		j+=1
+
+	if s != -1:
+		newname = i[:s] + fstr.format(int(item)) + i[e:]
+		print(i, '>>', newname)
+		store[i] = newname
+
 
 opt = input('\nProceed with renaming (y/n) ?\n> ')
 if opt.lower() == 'y':
