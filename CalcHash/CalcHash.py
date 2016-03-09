dc9eb8b3c8554ec291f0a782b9b6505ef5c584c0
aviaryan <avi.aryan123@gmail.com>
2016-03-09 10:08:31 +0530

    added CalcHash.py

---
+++ CalcHash/CalcHash.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import hashlib
+from sys import argv
+
+# FOR MD5, use inbuilt OS tool called md5sum
+
+
+def run(file):
+	if str(type(file)).find('list') > -1:
+		for i in file:
+			printHash(i)
+	else:
+		printHash(file)
+
+
+def printHash(file):
+	with open(file, "rb") as readfile:
+		data = readfile.read()
+		print(hashlib.sha256(data).hexdigest())
+
+
+if __name__ == '__main__':
+	if len(argv) > 1:
+		run(argv[1:])
+	else:
+		print('File Name > ', end='')
+		file = input()
+		run(file)
---
+++ CalcHash/README.md
@@ -0,0 +1,10 @@
+# CalcHash
+
+Calculates SHA256 of a file or list of files.
+
+For easy use in Windows, use a batch script in PATH directory.
+
+```batch
+@echo off
+python "C:\path\to\file\CalcHash.py" %1 %2 %3 %4 %5 %6
+```
